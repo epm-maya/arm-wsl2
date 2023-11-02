7a71883d2b7ba22386c0339484d19b37009d8c98
Noshiro Nanai <epm.maya@gmail.com>
2023-11-02 18:16:34 +0900

    slim (snapshot)

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -11,7 +11,7 @@ cd ..
 
 sudo ./alpine-make-rootfs \
 	--branch 'v3.18' \
-	--packages 'apk-tools curl tar xz zstd sudo vim git openssh-client' \
+	--packages 'apk-tools curl tar xz zstd' \
 	--script-chroot \
 	rootfs.tar.gz -- content/alpine.sh
 
